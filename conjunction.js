1e7a72f2982977a7059475e3f45ef5f5e80e3d42
Allison Collins <allisonkatecollins@gmail.com>
2018-12-19 15:08:14 -0600

    completed conjunction junction exercise

---
+++ conjunction.js
@@ -0,0 +1,7 @@
+//moved console.log inside curly braces
+const conjunction = function (firstWord, secondWord) {
+  const conjoinedWord = `${firstWord} ${secondWord}`;
+  console.log(conjoinedWord);
+}
+
+conjunction("Master", "Card");
